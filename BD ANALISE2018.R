135c279e031977b362a36a920be78896279a9db6
LuizaPinto <54412214+LuizaPinto@users.noreply.github.com>
2019-08-30 16:54:03 -0300

    Update BD ANALISE2018.R

--- BD ANALISE2018.R
+++ BD ANALISE2018.R
@@ -299,13 +299,30 @@ ggplot(BaseBrazil, aes(x = Weekly, y = TotalGross)) +
   labs(x='Meses', y='Bilheteria', title="Bilheteria mensal dos filmes no Brasil em 2018 em Milhões de Dólares") + coord_cartesian(ylim=c(20,60)) +
   theme_bw()
 
+####USA#####
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "USA")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
 
 
 
-######Grafico como fazer
+
+
+
 ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
-  geom_bar(stat = "identity")+
-  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes na  Argentina em 2018 em Milhões de Dólares")
+  geom_bar(stat = "identity", col = "black", fill = "steelblue")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes nos USA em 2018 em Milhões de Dólares")
+
+
 
 
 
