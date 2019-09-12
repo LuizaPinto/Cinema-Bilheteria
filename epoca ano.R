2fc4e227c33c9d56c9e894010755e7d2d84cffbc
LuizaPinto <54412214+LuizaPinto@users.noreply.github.com>
2019-09-12 13:20:48 -0300

    Add files via upload

---
+++ epoca ano.R
@@ -0,0 +1,163 @@
+####USA
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "USA")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
+
+
+
+
+
+
+ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+  geom_bar(stat = "identity", col = "black", fill = "darkblue")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes nos USA em 2018 em Milhões de Dólares")
+#####
+
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "Argentina")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
+
+
+
+
+
+
+ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+  geom_bar(stat = "identity", col = "yellow", fill = "skyblue")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes na Argentina em 2018 em Milhões de Dólares")
+#####Brasil
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "Brazil")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
+
+
+
+
+
+
+ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+  geom_bar(stat = "identity", col = "darkblue", fill = "forestgreen")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes no Brasil em 2018 em Milhões de Dólares")
+##### Germany
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "Germany")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
+
+
+
+
+
+
+ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+  geom_bar(stat = "identity", col = "black", fill = "darkgoldenrod")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes na Alemanha em 2018 em Milhões de Dólares")
+
+pal.heatmap
+#####China
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "China")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
+
+
+
+
+
+
+ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+  geom_bar(stat = "identity", col = "black", fill = "darkred")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes na China em 2018 em Milhões de Dólares")
+
+
+##South Africa
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "China")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
+
+
+
+
+
+
+ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+  geom_bar(stat = "identity", col = "black", fill = "springgreen4")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes na África do Sul em 2018 em Milhões de Dólares")
+
+
+
+###Australia
+base1 = BD_Bilheteria2_20_202018 %>% 
+  group_by(Country,Weekly) %>% 
+  summarise(TotalGross = sum(`Total Gross`/1000000))
+BaseBrazil = base1 %>% 
+  filter(Country == "Australia")
+BaseBrazil
+
+BaseBrazil$Weekly = ordered(BaseBrazil$Weekly, levels = c("January","February","March","April","May",
+                                                          "June","July",
+                                                          "August","September",
+                                                          "October","November",
+                                                          "December"))
+
+
+
+
+
+
+ggplot(BaseBrazil,aes(x = Weekly, y = TotalGross))+
+  geom_bar(stat = "identity", col = "red3", fill = "navy")+
+  labs(x = "Meses", y = "Bilheteria", title = "Bilheteria Mensal dos filmes na Australia em 2018 em Milhões de Dólares")
+
